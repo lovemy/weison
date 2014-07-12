bf725e919c00547768572db9b65af3c24ba3fe87
mahongbo <mhb@weison.(none)>
2014-07-12 17:38:54 +0800

    update the login page for frontend

---
+++ frontend/views/layouts/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <hr/>
+    <div class="container text-center">
+        <p class="copy">Copyright &copy; <?php echo date('Y'); ?> by <?php echo Core::getSiteParam('copyright');?>.<br/>
+	All Rights Reserved.</p>
+    </div>
+</div>
