bf65f07e3e819e91ddd0f4e046d8a8945aa2efae
hbma <hbma@terdon.com>
2014-07-16 12:50:01 +0800

    edit the header

---
+++ frontend/views/layouts/header.php
@@ -0,0 +1,11 @@
+<div id="navigation" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">	 
+	    <a class="navbar-brand" href="#"><img src="<?php echo Core::getSiteParam('frontend_logo');?>" style="height:30px;"/></a>
+	    <ul class="nav navbar-nav">
+	        <li class="active"><a href="#home" class="smothscroll">首 页</a></li>
+	        <li><a href="#desc" class="smothscroll">Description</a></li>
+	        <li><a href="#showcase" class="smothScroll">Showcase</a></li>
+	        <li><a href="#contact" class="smothScroll">Contact</a></li>
+	    </ul>
+    </div>
+</div>
