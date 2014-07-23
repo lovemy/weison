4e46be8714e4b0a91492a573d3d1de4ee3682ad2
root <root@weison.(none)>
2014-07-23 21:57:00 +0800

    update the frontend header

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -1,18 +1,43 @@
-<div id="navigation" class="navbar navbar-default navbar-fixed-top">
-    <div class="container">	 
-	    <a class="navbar-brand" href="<?php echo Yii::app()->createUrl('site/index');?>"><img src="<?php echo Core::getSiteParam('frontend_logo');?>" style="height:30px;"/></a>
-	    <ul class="nav navbar-nav">
-	        <li <?php if(Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'index'){?> class="active" <?php } ?>><a href="<?php echo Yii::app()->createUrl('site/index');?>" class="smothscroll">首 页</a></li>
-	        <li <?php if(Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'charts'){?> class="active" <?php } ?>><a href="<?php echo Yii::app()->createUrl('site/charts');?>" class="smothscroll">图表展示</a></li>
-	        <li <?php if(Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'help'){?> class="active" <?php } ?>><a href="<?php echo Yii::app()->createUrl('site/help');?>" class="smothscroll">帮助中心</a></li>	        
-	    </ul>
-	    <ul class="nav navbar-nav pull-right">
-	        <?php if(Yii::app()->user->isGuest){ ?>
-	    	<li><a href="<?php echo Yii::app()->createUrl('user/registration');?>" class="smothscroll">注 册</a></li>
-	        <li><a href="<?php echo Yii::app()->createUrl('user/login');?>" class="smothscroll">登 录</a></li>	        
-	        <?php } else{ ?>
-	        	<li><a href="<?php echo Yii::app()->createUrl('user/login');?>" class="smothscroll"><?php echo Yii::app()->user->name;?></a></li>	        	     
-	        <?php } ?>
-	    </ul>
-    </div>
-</div>
+<?php
+$this->widget(
+    'booster.widgets.TbNavbar',
+    array(
+        'type' => null, // null or 'inverse'
+        'brand' =>"&nbsp;&nbsp;&nbsp;&nbsp;".Core::getSiteParam('frontend_name'),
+        'brandUrl' => '#',
+        'collapse' => true, // requires bootstrap-responsive.css
+        'fixed' => true,
+        'fluid' => true,
+        'htmlOptions' => array('class'=>'navbar-fixed-top'),
+        'items' => array(
+            array(
+                'class' => 'booster.widgets.TbMenu',
+            	'type' => 'navbar',
+                'items' => array(
+                    array('label' => '首 页', 'url' => Yii::app()->createUrl('site/index'), 'active' => Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'index'),
+                    array('label' => '图 表', 'url' => Yii::app()->createUrl('site/charts'),'active' => Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'charts'),
+                    array('label' => '帮 助', 'url' => Yii::app()->createUrl('site/help'),'active' => Yii::app()->controller->id == 'site' && Yii::app()->controller->action->id == 'help'),
+                ),
+            ),
+            '<form class="navbar-form navbar-left" action=""><div class="form-group"><input type="text" class="form-control" placeholder="搜 索"></div></form>',
+            array(
+                'class' => 'booster.widgets.TbMenu',
+                'type' => 'navbar',
+                'htmlOptions' => array('class' => 'pull-right'),
+                'items' => array(
+                    array('label' => '登 录', 'url' => Yii::app()->createUrl('user/login'),'visible'=>Yii::app()->user->isGuest),
+                    array('label' => '注 册', 'url' => Yii::app()->createUrl('user/registration'),'visible'=>Yii::app()->user->isGuest),
+                    '---',
+                    array(
+                        'label' => Yii::app()->user->name,
+                        'url' => '#',
+                        'items' => array(
+                            array('label' => '退 出', 'url' => Yii::app()->createUrl('user/logout')),                           
+                        ),
+                        'visible'=>!Yii::app()->user->isGuest,
+                    ),                    
+                ),
+            ),
+        ),
+    )
+);
