fba56a53018dd95bac0e543b3a2bd48e10a49507
hbma <hbma@terdon.com>
2014-09-15 18:19:14 +0800

    update the user module

--- common/modules/user/views/profile/changepassword.php
+++ common/modules/user/views/profile/changepassword.php
@@ -1,45 +1,67 @@
 <?php $this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Change password");
+
 $this->breadcrumbs=array(
 	UserModule::t("Profile") => array('/user/profile'),
 	UserModule::t("Change password"),
 );
+
 $this->menu=array(
-	((UserModule::isAdmin())
-		?array('label'=>UserModule::t('Manage Users'), 'url'=>array('/user/admin'))
-		:array()),
-    array('label'=>UserModule::t('List User'), 'url'=>array('/user')),
-    array('label'=>UserModule::t('Profile'), 'url'=>array('/user/profile')),
-    array('label'=>UserModule::t('Edit'), 'url'=>array('edit')),
-    array('label'=>UserModule::t('Logout'), 'url'=>array('/user/logout')),
+	((UserModule::isAdmin())?array('label'=>UserModule::t('Manage Users'), 'url'=>array('/user/admin')):array()),
+	array('label'=>UserModule::t('List User'), 'url'=>array('/user')),
+	array('label'=>UserModule::t('Profile'), 'url'=>array('/user/profile')),
+	array('label'=>UserModule::t('Edit'), 'url'=>array('edit')),
+	array('label'=>UserModule::t('Logout'), 'url'=>array('/user/logout')),
 );
 ?>
 
-<h1><?php echo UserModule::t("Change password"); ?></h1>
+<div class="row">
+    	
+    	<div class="col-lg-3">
+	              <?php echo $this->renderPartial('common.modules.user.views.layouts.avatar_info',array()); ?>
+		<?php echo $this->renderPartial('common.modules.user.views.layouts.nav',array()); ?>	        	   
+	</div>
 
-<?php $form=$this->beginWidget('booster.widgets.TbActiveForm',array(
-	'id'=>'changepassword-form',
-	'enableAjaxValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+	<div class="col-lg-9">
+		<?php echo $this->renderPartial('common.modules.user.views.layouts.tab',array()); ?>
 
-<p class="help-block">带<span class="required">*</span>为必填内容.</p>
+		<h1><?php //echo UserModule::t("Change password"); ?></h1>
 
-<?php echo $form->errorSummary($model); ?>
-	
-	<?php echo $form->passwordFieldGroup($model,'oldPassword',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
-
-	<?php echo $form->passwordFieldGroup($model,'password',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
-
-	<?php echo $form->passwordFieldGroup($model,'verifyPassword',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
-	
-<div class="form-actions">
-	<?php $this->widget('booster.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'context'=>'primary',
-			'label'=>'保存修改',
+		<?php $form=$this->beginWidget('booster.widgets.TbActiveForm',array(
+			'id'=>'changepassword-form',
+			'enableAjaxValidation'=>true,
+			'type'=>'horizontal',
+			'clientOptions'=>array(
+				'validateOnSubmit'=>true,
+			),
 		)); ?>
-</div>
 
-<?php $this->endWidget(); ?>
+			<?php if(Yii::app()->user->hasFlash('profileMessage')){ ?>
+				<div class="alert in fade alert-success">
+					<a href="#" class="close" data-dismiss="alert">×</a>
+					<?php echo Yii::app()->user->getFlash('profileMessage');?>
+				</div>
+			<?php } ?>
+
+			<p class="help-block">带<span class="required">*</span>为必填内容.</p>
+
+			<?php echo $form->errorSummary($model); ?>
+	
+			<?php echo $form->passwordFieldGroup($model,'oldPassword',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
+
+			<?php echo $form->passwordFieldGroup($model,'password',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
+
+			<?php echo $form->passwordFieldGroup($model,'verifyPassword',array('widgetOptions'=>array('htmlOptions'=>array()))); ?>
+	
+			<div class="form-actions">
+				<?php $this->widget('booster.widgets.TbButton', array(
+					'buttonType'=>'submit',
+					'htmlOptions'=>array('style'=>'margin-left:220px;'),
+					'context'=>'primary',
+					'label'=>'保存修改',
+				)); ?>
+			</div>
+
+		<?php $this->endWidget(); ?>
+
+	</div>
+</div>
