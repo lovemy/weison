a42db0ca0d74d9deff1b8e96be1abf9dee564893
mahongbo <xiaoma_1102@163.com>
2014-09-22 20:18:27 +0800

    Update Core.php

--- common/components/Core.php
+++ common/components/Core.php
@@ -17,4 +17,26 @@ class Core{
 		$user = User::model()->findByPk($user_id);
 		return $user;
 	}
+	
+	
+	public static function getPassed($update_time)
+	{
+		$now_str = time();
+		$update_str = strtotime($update_time);
+		$passed_str = abs($now_str - $update_str);		
+		if($passed_str<60){
+			$res = $passed_str."秒前";
+		}else if($passed_str>=60 && $passed_str<60*60){
+			$res = floor($passed_str/60)."分钟前";
+		}else if($passed_str>=60*60 && $passed_str<60*60*24){
+			$res = floor($passed_str/(60*60))."小时前";
+		}else if($passed_str>=60*60*24 && $passed_str<60*60*24*30){
+			$res = floor($passed_str/(60*60*24))."天前";
+		}else if($passed_str>=60*60*24*30 && $passed_str<60*60*24*30*12){
+			$res = floor($passed_str/(60*60*24*30))."个月前";
+		}else{
+			$res = date("Y年m月d日",strtotime($update_time));
+		}
+		return $res;
+	}
 }
